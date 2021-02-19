cd601d998a7c6a1b7094a5e24f641c33f9759b56
Adam Mertzenich <adam@mertzeni.ch>
2021-02-19 12:08:55 -0600

    switch to tuples

---
+++ src/projects/keyboard/testbed.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+first_keyboard = {
+    3: ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
+    2: ["a", "s", "d", "f", "g", "h", "j", "k", "l"],
+    1: ["z", "x", "c", "v", "b", "n", "m"]
+}
+
+
+second_keyboard = {
+    3: ("q", "w", "e", "r", "t", "y", "u", "i", "o", "p"),
+    2: ("a", "s", "d", "f", "g", "h", "j", "k", "l"),
+    1: ("z", "x", "c", "v", "b", "n", "m")
+}
+
+def find_axis(key: str):
+    """Find X Axis of a Character"""
+    for i in second_keyboard:
+        if key in second_keyboard[i]:
+            return second_keyboard[i].index(key), i
+    return False
+
+print(find_axis("w"))
