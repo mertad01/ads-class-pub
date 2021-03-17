9eb5a373b933927d61081dd5c4e2e9fb9968b879
Adam Mertzenich <mertad01@luther.edu>
2021-03-17 10:11:08 -0500

    Recursion GCD

--- src/exercises/recursion/recursion_functions.py
+++ src/exercises/recursion/recursion_functions.py
@@ -1,37 +1,50 @@
 #!/usr/bin/env python3
 """
-Exercise `recursion` implementation
+`recursion` implementation
 
-@authors:
+@author:
 """
+# function from fractions.py:
+def old_gcd(num_a: int, num_b: int) -> int:
+    """
+    Greatest Common Denominator of two integers
+
+    Helper function to simplify fractions
+    """
+    while num_a % num_b:
+        num_a, num_b = num_b, num_a % num_b
+    return num_b
 
 
-def gcd(a: int, b: int) -> int:
-    raise NotImplementedError
-
-
-def hourglass_ite(levels: int) -> None:
-    raise NotImplementedError
+def gcd(num_one: int, num_two: int) -> int:
+    """Greatest Common Denominator"""
+    if not num_one % num_two:
+        return num_two
+    return gcd(num_two, num_one % num_two)
 
 
 def diamond_ite(levels: int) -> None:
-    raise NotImplementedError
-
-
-def hourglass_rec(levels: int) -> None:
+    """Print a diamond"""
     raise NotImplementedError
 
 
 def diamond_rec(levels: int) -> None:
+    """Print a diamond"""
+    raise NotImplementedError
+
+
+def hourglass_ite(levels: int) -> None:
+    """Print an hourglass"""
+    raise NotImplementedError
+
+
+def hourglass_rec(levels: int) -> None:
+    """Print an hourglass"""
     raise NotImplementedError
 
 
 def main():
-    """Main function"""
-    hourglass_ite(5)
-    hourglass_rec(5)
-    diamond_ite(5)
-    diamond_rec(5)
+    """Main"""
 
 
 if __name__ == "__main__":
