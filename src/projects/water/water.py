3a2695f0199900e5d8da5a3bcba42dc2f7979965
Adam Mertzenich <mertad01@luther.edu>
2021-03-22 17:00:39 -0500

    Water project initial State class

--- src/projects/water/water.py
+++ src/projects/water/water.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
-"""Water jugs project"""
+"""
+`water` implementation
 
+@authors: Adam Mertzenich
+"""
 
 JUG_1_MAX = 5
 JUG_2_MAX = 3
@@ -11,57 +14,96 @@ class State:
 
     def __init__(self, jug_1: int, jug_2: int):
         """__init__"""
-        raise NotImplementedError
+        if not isinstance(jug_1, int):
+            raise TypeError("__init__() should return None, not 'NotImplementedType'")
+        if not isinstance(jug_2, int):
+            raise TypeError("__init__() should return None, not 'NotImplementedType'")
+        if jug_1 > JUG_1_MAX:
+            raise ValueError(
+                "The proposed value exceeds jug 1 capacity"
+            )
+        if jug_2 > JUG_2_MAX:
+            raise ValueError(
+                "The proposed value exceeds jug 2 capacity"
+            )
+        self._jug_1 = jug_1
+        self._jug_2 = jug_2
 
-    def __eq__(self, other: object):
+    def __eq__(self, other: object) -> bool:
         """__eq__"""
-        raise NotImplementedError
+        if str(self) == str(other):
+            return True
+        return False
 
-    def __str__(self):
+    def __repr__(self) -> str:
+        """__repr__"""
+        return f'({self._jug_1}, {self._jug_2})'
+
+    def __str__(self) -> str:
         """__str__"""
-        raise NotImplementedError
+        return f'({self._jug_1}, {self._jug_2})'
 
     def clone(self):
         """Copy a state"""
-        raise NotImplementedError
+        return State(self._jug_1, self._jug_2)
 
     def fill_jug_1(self):
         """Fill jug1 to capacity from the pump"""
-        raise NotImplementedError
+        self._jug_1 = JUG_1_MAX
 
     def fill_jug_2(self):
         """Fill jug2 to capacity from the pump"""
-        raise NotImplementedError
+        self._jug_2 = JUG_2_MAX
 
     def empty_jug_1(self):
         """Pour the water from jug1 onto the ground"""
-        raise NotImplementedError
+        self._jug_1 = 0
 
     def empty_jug_2(self):
         """Pour the water from jug2 onto the ground"""
-        raise NotImplementedError
+        self._jug_2 = 0
 
     def pour_jug_1_to_jug_2(self):
         """Pour as much water as you can from jug1 to jug2 without spilling"""
-        raise NotImplementedError
+        jug_one = self._jug_1
+        jug_two = self._jug_2
+
+        if jug_one + jug_two > JUG_2_MAX:
+            self._jug_1 = jug_two - (JUG_2_MAX - jug_one)
+            self._jug_2 = JUG_2_MAX
+        elif jug_one == 0:
+            pass
+        else:
+            self._jug_2 += jug_one
+            self._jug_1 -= jug_one
 
     def pour_jug_2_to_jug_1(self):
         """Pour as much water as you can from jug2 to jug1 without spilling"""
-        raise NotImplementedError
+        jug_one = self._jug_1
+        jug_two = self._jug_2
+
+        if jug_one + jug_two > JUG_1_MAX:
+            self._jug_1 = JUG_1_MAX
+            self._jug_2 = jug_one - (JUG_1_MAX - jug_two)
+        elif jug_two == 0:
+            pass
+        else:
+            self._jug_1 += jug_two
+            self._jug_2 -= jug_two
 
 
-def search(start_state: object, goal: object, moves_lst: list):
+def search(start_state: State, goal: State, moves_lst: list):
     """Find a sequence of states"""
     raise NotImplementedError
 
 
 def main():
-    """Main function"""
-    goal = State(4, 0)
-    start = State(0, 0)
-    moves = []
-    search(start, goal, moves)
-    print(", ".join([str(s) for s in moves]))
+    """Main"""
+    # goal = State(4, 0)
+    # start = State(0, 0)
+    # moves = []
+    # search(start, goal, moves)
+    # print(", ".join([str(s) for s in moves]))
 
 
 if __name__ == "__main__":
