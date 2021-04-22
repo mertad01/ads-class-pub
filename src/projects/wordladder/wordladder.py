e55886fd0c49915bce73e9b1da2ca7377f4c2ce9
Adam Mertzenich <mertad01@luther.edu>
2021-04-22 15:33:05 -0500

    Pass all tests

--- src/projects/wordladder/wordladder.py
+++ src/projects/wordladder/wordladder.py
@@ -6,6 +6,7 @@
 @version: 2021.4
 """
 
+import copy
 from collections import deque
 from typing import List, Set
 
@@ -42,12 +43,16 @@ class Solver:
         if len(word1) != len(word2):
             raise ValueError("Must use words of the same length")
 
-        dist = 0
-        for count, i in enumerate(word1):
-            if i != word2[count]:
-                dist += 1
+        # Old Solution:
+        # dist = 0
+        # for count, i in enumerate(word1):
+        #     if i != word2[count]:
+        #         dist += 1
 
-        return dist
+        # List Comprehension
+        return len(
+            [i for i in zip(word1, word2) if i[0] != i[1]]
+        )
 
     def diff_by_one_all(
         self, word: str, all_words: Set[str], used_words: Set[str]
@@ -59,7 +64,9 @@ class Solver:
         @param all_words: all words of the same length as `word`
         @param used_words: all words that are already used and should not be considered
         """
+
         output: List[str] = list()
+        # used_words.add(word)
         for i in all_words:
             distance = self.distance(word, i)
             if distance == 1 and i not in used_words:
@@ -76,19 +83,72 @@ class Solver:
         @param word_stop: 2nd word
         @return a word ladder as a list
         """
-        raise NotImplementedError
+        used_words = []
+        used_words.append(word_start)
+        queue: deque = deque()
+        stack = deque
+
+        word_size = len(word_start)
+        if word_size == 3:
+            all_words = self.words3
+        elif word_size == 4:
+            all_words = self.words4
+        else:
+            all_words = self.words5
+
+        for count, i in enumerate(
+                self.diff_by_one_all(word_start, all_words, set(used_words))
+        ):
+            queue.append(stack())
+            queue[count].appendleft(word_start)
+            queue[count].appendleft(i)
+            used_words.append(i)
+
+        running = len(queue)
+        while running > 0:
+            item = queue.popleft()
+            if item[0] == word_stop:
+                return item
+            else:
+                unused_words = self.diff_by_one_all(item[0], all_words, set(used_words))
+                for i in unused_words:
+                    clone = copy.deepcopy(item)
+                    clone.appendleft(i)
+                    used_words.append(i)
+                    queue.append(clone)
+            running = len(queue)
+
+        # print(used_words)
+        # print(
+        #     self.diff_by_one_all(
+        #         "elf",
+        #         self.words3,
+        #         {
+        #             "elk", "elb", "elf",
+        #             "eli", "ilk", "elt",
+        #             "alk", "elm", "eld",
+        #             "els", "ell", "ebb",
+        #             "alb"
+        #         }
+        #     )
+        # )
 
 
 def main():
     """Main"""
     solver = Solver("/home/adam/Documents/CS-160/ads-class-pub/data/projects/wordladder/words.txt")
+
     print(
-        solver.diff_by_one_all(
-            "elk",
-            {"elf", "eld", "ilk", "elt", "eli", "els", "alk", "elm", "elb", "ell"},
-            {"elf", "ilk", "eli", "elm", "elb"}
-        )
+        solver.build_ladder("elk", "elf")
     )
+
+    # print(
+    #     solver.diff_by_one_all(
+    #         "elk",
+    #         {"elf", "eld", "ilk", "elt", "eli", "els", "alk", "elm", "elb", "ell"},
+    #         {"elf", "ilk", "eli", "elm", "elb"}
+    #     )
+    # )
 
 
 if __name__ == "__main__":
